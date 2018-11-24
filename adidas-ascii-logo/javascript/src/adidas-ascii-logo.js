e038bd67138a251e33dd1a25d3c67914c749c534
Jose Moros <jmoros@avantio.com>
2018-11-24 14:41:29 +0100

    🐲 master: solved adidas ascii logo challenge

--- adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
+++ adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
@@ -3,6 +3,29 @@
  * @param {number} width - Width of a stripe.
  * @returns {string} adidas logo.
  */
-module.exports = function(width) {
-  return `${ width }`;
+
+const STRIPES = 3;
+
+module.exports = (width) => {
+  if (width < 2) {
+    throw new Error('Error, minimun width is 2');
+  }
+
+  const separation = Math.round(Math.sqrt(width));
+  const logoChars = '@'.repeat(width);
+  const spaces = ' '.repeat(separation);
+  const logo = [];
+
+  for (let i = 1; i <= STRIPES; i++) {
+    const paddingLength = (STRIPES - i) * width;
+
+    for (let j = 0; j < separation; j++) {
+      const leftPadding = ' '.repeat(paddingLength + j);
+      const logoPart = Array(i).fill(logoChars).join(spaces);
+
+      logo.push(`${leftPadding}${logoPart}`);
+    }
+  }
+
+  return logo.join('\n');
 };
